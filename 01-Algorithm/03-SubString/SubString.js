c6eea237a649c84b36443fefe1afaa0e2327ad8e
Stephen Tian <745752067@qq.com>
2018-09-09 22:50:18 +0800

    Day 3； SubString

---
+++ 01-Algorithm/03-SubString/SubString.js
@@ -0,0 +1,17 @@
+/**
+ * author: stephentian
+ * email: stephentian@foxmail.com
+ * day: 2018-9-9
+ **/
+
+// JS截取字符串可使用 substring()或者slice()
+
+// substring: substring(start,end)表示从start到end之间的字符串，包括start位置的字符但是不包括end位置的字符。
+
+// substr: substr(start,length)表示从start位置开始，截取length长度的字符串。
+
+// split: split('')使用一个指定的分隔符把一个字符串分割存储到数组
+
+// join： join('')使用一个指定的分隔符把一个字符串合并为同一个字符串
+
+// slice: slice(start,end)返回一个新的数组，包含从 start 到 end （不包括该元素）的 arrayObject 中的元素。
