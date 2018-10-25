c0d6d9e7a90d6c73d588497cf0abfb41563f007f
Stephen Tian <745752067@qq.com>
2018-10-25 21:42:46 +0800

    添加 Promise 学习步骤，附带 README 文件

--- 04-Promise/01-Chaining.js
+++ 04-Promise/01-Chaining.js
@@ -4,15 +4,13 @@
  * day: 2018-10-25
  **/
 
-// 链式调用
-
+// 1. 链式调用
 // const promise = doSomething()
 // const promise2 = promise.then(successCallback, failureCallback)
 // 或者
 // const promise2 = doSomething().then(successCallback, failreCallback)
 
 // 在过去
-
 // doSomething(function(result) {
 //   doSomethingElse(result, function(newResult) {
 //     doThirdThing(newResult, function(finalResult) {
@@ -22,7 +20,6 @@
 // }, failureCallback);
 
 // 通过新式函数, then里的参数是可选的
-
 // doSomething().then(function(result) {
 //   return doSomethingElse(result);
 // })
@@ -34,19 +31,71 @@
 // })
 // .catch(failureCallback);
 
-new Promise((resolve, reject) => {
-  console.log('Initial')
 
-  resolve()
-})
-  .then(() => {
-    throw new Error('Something failed')
+// 2. Catch 的后续操作
+// new Promise((resolve, reject) => {
+//     console.log('Initial')
+//     resolve()
+//   })
+//   .then(() => {
+//     throw new Error('Something failed')
 
-    console.log('Do this')
-  })
-  .catch(() => {
-    console.log('Do that')
-  })
-  .then(() => {
-    console.log('Do this whatever happened before')
-  })
+//     console.log('Do this')
+//   })
+//   .catch(() => {
+//     console.log('Do that')
+//   })
+//   .then(() => {
+//     console.log('Do this whatever happened before')
+//   })
+
+
+// 3. 创建一个 Promise
+// const myFirstPromise = new Promise((resolve, reject) => {
+// 做一些异步操作,最终会调用下面两者之一
+// resolve(someValue)
+// 或
+// reject("failure reason")
+// })
+
+// 让某个函数有 Promise 功能，只需返回一个 Promise 即可
+// 比如封装一个 get 请求
+// function myAsyncFunc(url) {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest()
+//     xhr.open("GET", url)
+//     xhr.onload = () => resolve(xhr.responseText)
+//     xhr.onerror = () => reject(xhr.statusText)
+//     xhr.send()
+//   })
+// }
+
+
+// 4. 解决回调地狱
+// let fs = require('fs')
+// fs.readFile('./a.txt','utf8',function(err,data){
+//   fs.readFile(data,'utf8',function(err,data){
+//     fs.readFile(data,'utf8',function(err,data){
+//       console.log(data)
+//     })
+//   })
+// })
+
+// 使用 Promise 后
+// let fs = require('fs')
+// function readFile(url) {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(url, 'utf-8', function(error, data) {
+//       error && reject(error)
+//       resolve(data)
+//     })
+//   })
+// }
+
+// readFile('./a.txt').then(data => {
+//   return readFile(data)
+// }).then(data => {
+//   return readFile(data)
+// }).then(data => {
+//   console.log(data)
+// })
