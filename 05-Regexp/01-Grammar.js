c70720ec3173b077ea13a1eccf562633f29aeb0a
Stephen Tian <745752067@qq.com>
2018-11-05 22:54:27 +0800

    正则 添加 正则对象方法 test， exec

--- 05-Regexp/01-Grammar.js
+++ 05-Regexp/01-Grammar.js
@@ -3,3 +3,22 @@
  * email: stephentian@foxmail.com
  * day: 2018-11-2
  **/
+
+var regex1 = /.a/;
+var string1 = 'ba';
+var string2 = ' a';
+var string3 = '@abc';
+var string4 = `
+abc
+xyz
+`;
+
+console.log(regex1.test(string1));
+// true
+console.log(regex1.test(string2));
+// true
+console.log(regex1.test(string3));
+// true
+
+console.log(regex1.test(string4));
+// false 无法匹配换行符
