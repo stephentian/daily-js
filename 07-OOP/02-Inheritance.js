d622ea5fe5a2b1cf24f57add56a3f48deecc8baf
Stephen Tian <745752067@qq.com>
2018-11-21 21:29:47 +0800

    the same way to achive New

--- 07-OOP/02-Inheritance.js
+++ 07-OOP/02-Inheritance.js
@@ -3,3 +3,21 @@
  * email: stephentian@foxmail.com
  * day: 2018/11/21
  **/
+
+//  1. New
+function Animals(name) {
+  this.name = name
+}
+
+var animal1 = new Animals('animal1')
+
+console.log('animal1:', animal1)
+
+// 2. New 相当于如下
+var animal2 = {}
+
+animal2.__proto__ = Animals.prototype
+
+Animals.call(animal2, 'animal2')
+
+console.log('animal2:', animal2)
