0591f68761cd43431379a3ca7549605dafe234d1
Stephen Tian <745752067@qq.com>
2019-02-27 23:38:38 +0800

    lodash afeter

--- 04-Promise & Async+Await/01-Generator.js
+++ 04-Promise & Async+Await/01-Generator.js
@@ -21,7 +21,9 @@ console.log(isArray([]))
 
 // 思考：能不能写一个方法直接批量返回这些函数呢？
 
+
 // 二、
+// 写一个函数判断类型, 通过它批量生成函数
 function isType(type) {
   return function (param) {
     return Object.prototype.toString.call(param) === `[object ${type}]`
@@ -32,3 +34,40 @@ let isString1 = isType('String')
 let isArray1 = isType('Array')
 console.log(isString1('11'))
 console.log(isArray1([1]))
+
+// 类似函数工厂, 生产函数
+// 这其实也是闭包的应用, 当函数执行时, 会产生私有作用域, 上下文执行环境, 可以一直引用里面的 type
+
+
+// 三、
+// lodash 中有个 after 方法
+// 作用: 指定一个函数被调用多少次才会执行
+function sleep() {
+  console.log('睡着了')
+}
+
+// 函数 after
+// 接受参数 times 和 fn(函数)
+// 返回一个新的函数, 当新的函数执行 times 次后, 执行 fn
+// function after(times, fn) {
+//   ...
+// }
+// let newSleep = after(3, sleep)
+// newSleep()
+// newSleep()
+// newSleep() // 执行 sleep
+
+// 思考:  after 里的内容
+
+function after(times, fn) {
+  let count = 1
+  return function () {
+    if (count++ == times) {
+      fn()
+    }
+  }
+}
+let newSleep = after(3, sleep)
+newSleep()
+newSleep()
+newSleep()
