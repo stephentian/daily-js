86f1eb471cc8dac8007dbb3f24dabaca873edce8
Stephen Tian <745752067@qq.com>
2019-03-08 23:34:13 +0800

    es5 && es6 apply

---
+++ 08-This/apply.js
@@ -0,0 +1,39 @@
+/**
+* author: stephentian
+* email: stephentian@foxmail.com
+* day: 2019-3-8
+**/
+
+// apply
+
+Function.prototype.myApply = function (context) {
+  context = context || window
+  context.fn = this
+  let result
+  if (arguments[1]) {
+    result = context.fn(...arguments[1])
+  } else {
+    result = context.fn()
+  }
+  delete context.fn
+  return result
+}
+
+// ES5
+Function.prototype.myApply2 = function (obj, arr) {
+  obj = obj || window
+  let result
+  let argsLength = arguments.length
+  obj.fn = this
+  if (!arr) {
+    result = obj.fn()
+  } else {
+    let args = []
+    for (let i = 1; i < argsLength; i++) {
+      args.push('arguments[' + i + ']')
+    }
+    result = eval('obj.fn(' + args + ')')
+  }
+  delete obj.fn
+  return result
+}
