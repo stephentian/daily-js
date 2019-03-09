2174d0861ed15ab30768aa8a4b4b3e276d754e24
Stephen Tian <745752067@qq.com>
2019-03-09 22:56:21 +0800

    es6 bind

---
+++ 08-This/bind.js
@@ -0,0 +1,10 @@
+Function.prototype.myBind = function (context) {
+  const self = this,
+    args = [...arguments].slice(1)
+  return function F() {
+    if (this instanceof F) {
+      return new self(...args, ...arguments)
+    }
+    return self.apply(context, args.concat(...arguments))
+  }
+}
