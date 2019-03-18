ce391ca9f224df9224287cd01e90019d32a6cca3
Stephen Tian <745752067@qq.com>
2019-03-18 11:22:47 +0800

    template string

---
+++ 00-ES6/02-Template_string/index.js
@@ -0,0 +1,22 @@
+/**
+* author: stephentian
+* email: stephentian@foxmail.com
+* day: 2019-3-18
+**/
+
+// 模板字符串
+
+// js
+
+// var name = 'stephent'
+// var age = 18
+// var html = ''
+
+// html = 'My name is <b>' + name + '</b>,' + 'I\'m' + age + 'years old.'
+
+// ES6
+const name = 'stephen'
+const age = 18
+const html = `My name is <b>${name}</b> , I'm ${age} years old.`
+
+console.log(html)
