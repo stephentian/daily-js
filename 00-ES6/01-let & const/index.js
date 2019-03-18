ab3608cf5c5d64a279394acdf2f99469dbcc1b74
Stephen Tian <745752067@qq.com>
2019-03-18 09:35:43 +0800

    es6 let&const

---
+++ 00-ES6/01-let & const/index.js
@@ -0,0 +1,17 @@
+/**
+* author: stephentian
+* email: stephentian@foxmail.com
+* day: 2019-3-18
+**/
+
+// js
+// var i = 10
+// i = 100
+// var j = 20
+// j = 200
+
+// es6
+let i = 10
+i = 100 // 正确
+const j = 20
+j = 200 // 报错
