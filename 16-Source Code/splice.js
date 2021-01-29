ed2a0ce109e1ce7199e2591af549592517e7f31a
stepentian <745752067@qq.com>
2021-01-29 09:25:52 +0800

    feat: splice

--- 16-Source Code/splice.js
+++ 16-Source Code/splice.js
@@ -72,8 +72,27 @@ function SparseSlice(array, start_i, del_count, deleted_elements) {
 
 // 移动元素
 // SparseMove
-function SparseMove(array, start_i, del_count, item) {
+function SparseMove(array, start_i, del_count, num_elements_to_add) {
+    if (num_elements_to_add === del_count)  return
 
+    let new_array = new Array(array.length - del_count + num_elements_to_add)
+
+    for (let i = 0; i < new_array.length; i++) {
+        if (i < start_i) {
+            new_array[i] = array[i]
+        } else if (num_elements_to_add < del_count && i >= start_i + (del_count - num_elements_to_add)) {
+            // 如果 添加数目 < 删除数目，i 填充位为 start_i + (del_count - num_elements_to_add)
+            new_array[i] = array[i+(del_count - num_elements_to_add)]
+        } else if (num_elements_to_add > del_count && i >= start_i + num_elements_to_add) {
+            new_array[i] = array[i-(num_elements_to_add - del_count)]
+        } else {
+            continue
+        }
+    }
+    console.log('new_array：', new_array)
+
+    // array = new_array
+    return new_array
 }
 
 function ArraySplice(start, delete_count, ...item) {
@@ -87,16 +106,31 @@ function ArraySplice(start, delete_count, ...item) {
     let deleted_elements = new Array(del_count)
     let num_elements_to_add = item ? item.length : 0
 
-    SparseSlice(array, start_i, del_count, deleted_elements)
-    SparseMove(array, start_i, del_count, item)
+    console.log('array: ', array)
+    console.log('len: ', len)
+    console.log('start_i: ', start_i)
+    console.log('del_count: ', del_count)
+    console.log('num_elements_to_add: ', num_elements_to_add)
 
-    // 添加元素
+    SparseSlice(array, start_i, del_count, deleted_elements)
+    array = SparseMove(array, start_i, del_count, num_elements_to_add)
+
+    // 添加/替换元素
     let i = start_i
     let j = 0
     while (j < item.length) {
         array[i++] = item[j++]
     }
 
+    console.log('array: ', array)
+    console.log('deleted_elements: ', deleted_elements)
 
     return deleted_elements
 }
+
+const months = ['Jan', 'March', 'April', 'June']
+
+var splice1 = ArraySplice.bind(months)
+
+splice1(1, 0, 'Feb')
+splice1(4, 1, 'May')
